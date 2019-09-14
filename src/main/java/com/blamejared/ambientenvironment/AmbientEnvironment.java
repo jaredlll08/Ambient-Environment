764d124bfc21c3bdb05fbf3fd3527997e61c7d81
Jared <jaredlll08@gmail.com>
2019-09-14 03:12:25 +0200

    new algo, version push

--- src/main/java/com/blamejared/ambientenvironment/AmbientEnvironment.java
+++ src/main/java/com/blamejared/ambientenvironment/AmbientEnvironment.java
@@ -1,9 +1,12 @@
 package com.blamejared.ambientenvironment;
 
 import net.minecraft.world.biome.BiomeColorHelper;
+import net.minecraft.world.gen.NoiseGeneratorPerlin;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.relauncher.*;
+
+import java.util.Random;
 
 @Mod(modid = "ambientenvironment", name = "Ambient Environment", version = "1.0.1", clientSideOnly = true)
 public class AmbientEnvironment {
@@ -18,43 +21,41 @@ public class AmbientEnvironment {
         BiomeColorHelper.ColorResolver grassColor = BiomeColorHelper.GRASS_COLOR;
         BiomeColorHelper.ColorResolver waterColor = BiomeColorHelper.WATER_COLOR;
         BiomeColorHelper.ColorResolver foliageColor = BiomeColorHelper.FOLIAGE_COLOR;
+    
+        int levels = 2;
+        NoiseGeneratorPerlin NOISE_GRASS = new NoiseGeneratorPerlin(new Random("NOISE_GRASS".hashCode()), levels);
+        NoiseGeneratorPerlin NOISE_WATER = new NoiseGeneratorPerlin(new Random("NOISE_WATER".hashCode()), levels);
+        NoiseGeneratorPerlin NOISE_FOLIAGE = new NoiseGeneratorPerlin(new Random("NOISE_FOLIAGE".hashCode()), levels);
+    
         
         BiomeColorHelper.GRASS_COLOR = (biome, pos) -> {
-            float scale = 5f;
-            double value = ((NOISE_GRASS.eval(pos.getX() / scale, pos.getZ() / scale)));
-            value = remap(value, -1, 1, 0, 0.15);
-            double val = value;
-            int moddedBiomeGrassColor = grassColor.getColorAtPos(biome, pos);
-            float[] argb = getARGB(moddedBiomeGrassColor);
-            argb[1] *= 0.9 + val;
-            argb[2] *= 0.9 + val;
-            argb[3] *= 0.9 + val;
-            return toInt(argb);
+            int newColor = grassColor.getColorAtPos(biome, pos);
+            float scale = 8f;
+            double value = ((NOISE_GRASS.getValue(pos.getX() / scale, pos.getZ() / scale)));
+            double darkness = 0.25f;
+            value = curve(0, 1, remap(value, -((1 << levels) - 1), (1 << levels) - 1, 0, 1), 1) * darkness;
+            return blend(newColor, 0, (float) (value));
         };
         BiomeColorHelper.WATER_COLOR = (biome, pos) -> {
             int newColor = waterColor.getColorAtPos(biome, pos);
-            float scale = 8f;
-            double value = ((NOISE_WATER.eval(pos.getX() / scale, pos.getZ() / scale)));
-            value = remap(value, -1, 1, 0, 0.12);
-            double val = value;
-            float[] argb = getARGB(newColor);
-            argb[1] *= 0.88 + val;
-            argb[2] *= 0.88 + val;
-            argb[3] *= 0.88 + val;
-            return toInt(argb);
+            float scale = 16f;
+            double value = ((NOISE_WATER.getValue(pos.getX() / scale, pos.getZ() / scale)));
+            double darkness = 0.3f;
+            value = curve(0, 1, remap(value, -((1 << levels) - 1), (1 << levels) - 1, 0, 1), 1) * darkness;
+            return blend(newColor, 0, (float) (value));
         };
         BiomeColorHelper.FOLIAGE_COLOR = (biome, pos) -> {
-            int moddedBiomeFoliageColor = foliageColor.getColorAtPos(biome, pos);
-            float scale = 5f;
-            double value = ((NOISE_FOLIAGE.eval(pos.getX() / scale, pos.getZ() / scale)));
-            value = remap(value, -1, 1, 0, 0.3);
-            double val = value;
-            float[] argb = getARGB(moddedBiomeFoliageColor);
-            argb[1] *= 0.7 + val;
-            argb[2] *= 0.7 + val;
-            argb[3] *= 0.7 + val;
-            return toInt(argb);
+            int newColor = foliageColor.getColorAtPos(biome, pos);
+            float scale = 8f;
+            double value = ((NOISE_FOLIAGE.getValue(pos.getX() / scale, pos.getZ() / scale)));
+            double darkness = 1;
+            value = curve(0, 1, remap(value, -((1 << levels) - 1), (1 << levels) - 1, 0, 1), 1) * darkness;
+            return blend(newColor, 0, (float) (value));
         };
+    }
+    
+    public static double remap(double value, double currentLow, double currentHigh, double newLow, double newHigh) {
+        return newLow + (value - currentLow) * (newHigh - newLow) / (currentHigh - currentLow);
     }
     
     private static float getRed(int hex) {
@@ -73,20 +74,35 @@ public class AmbientEnvironment {
         return ((hex >> 24) & 0xff) / 255f;
     }
     
-    
     private static float[] getARGB(int hex) {
         return new float[] {getAlpha(hex), getRed(hex), getGreen(hex), getBlue(hex)};
     }
     
     private static int toInt(float[] argb) {
-        int r = Math.round(argb[1] * 255) & 0xFF;
-        int g = Math.round(argb[2] * 255) & 0xFF;
-        int b = Math.round(argb[3] * 255) & 0xFF;
-        int a = Math.round(argb[0] * 255) & 0xFF;
+        int r = (int) Math.floor(argb[1] * 255) & 0xFF;
+        int g = (int) Math.floor(argb[2] * 255) & 0xFF;
+        int b = (int) Math.floor(argb[3] * 255) & 0xFF;
+        int a = (int) Math.floor(argb[0] * 255) & 0xFF;
         return (a << 24) + (r << 16) + (g << 8) + (b);
     }
     
-    public static double remap(double value, double currentLow, double currentHigh, double newLow, double newHigh) {
-        return newLow + (value - currentLow) * (newHigh - newLow) / (currentHigh - currentLow);
+    public static double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(value, max));
+    }
+    
+    public static double curve(double start, double end, double amount, double waves) {
+        amount = clamp(amount, 0, 1);
+        amount = clamp((amount - start) / (end - start), 0, 1);
+        return clamp(0.5 + 0.5 * Math.sin(Math.cos(Math.PI * Math.tan(90 * amount))) * Math.cos(Math.sin(Math.tan(amount))), 0, 1);
+    }
+    
+    public static int blend(int color1, int color2, float ratio) {
+        float ir = 1.0f - ratio;
+        
+        float[] rgb1 = getARGB(color2);
+        float[] rgb2 = getARGB(color1);
+        
+        return toInt(new float[] {rgb1[0] * ratio + rgb2[0] * ir, rgb1[1] * ratio + rgb2[1] * ir, rgb1[2] * ratio + rgb2[2] * ir, rgb1[3] * ratio + rgb2[3] * ir});
+        
     }
 }
