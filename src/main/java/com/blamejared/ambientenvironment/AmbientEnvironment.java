ec115dc2a169ad99cfce13958fd3e294ce331757
Jared <jaredlll08@gmail.com>
2020-07-12 21:01:38 +0200

    Auto push to curse and version update tracker

--- src/main/java/com/blamejared/ambientenvironment/AmbientEnvironment.java
+++ src/main/java/com/blamejared/ambientenvironment/AmbientEnvironment.java
@@ -12,21 +12,21 @@ import java.util.Random;
 
 @Mod("ambientenvironment")
 public class AmbientEnvironment {
-    
+
     public AmbientEnvironment() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     }
-    
+
     private void doClientStuff(final FMLClientSetupEvent event) {
         ColorResolver grassColor = BiomeColors.GRASS_COLOR;
         ColorResolver waterColor = BiomeColors.WATER_COLOR;
 //        BiomeColors.IColorResolver foliageColor = BiomeColors.FOLIAGE_COLOR;
-        
+
         int levels = 2;
         PerlinNoiseGenerator NOISE_GRASS = new PerlinNoiseGenerator(new SharedSeedRandom("NOISE_GRASS".hashCode()), levels, 0);
         PerlinNoiseGenerator NOISE_WATER = new PerlinNoiseGenerator(new SharedSeedRandom("NOISE_WATER".hashCode()), levels, 0);
 //        PerlinNoiseGenerator NOISE_FOLIAGE = new PerlinNoiseGenerator(new Random("NOISE_FOLIAGE".hashCode()), levels);
-        
+
         BiomeColors.GRASS_COLOR = (biome, posX, posZ) -> {
             int newColor = grassColor.getColor(biome, posX, posZ);
             float scale = 8f;
@@ -52,31 +52,31 @@ public class AmbientEnvironment {
 //            return blend(newColor, 0, (float) (value));
 //        };
     }
-    
+
     public static double remap(double value, double currentLow, double currentHigh, double newLow, double newHigh) {
         return newLow + (value - currentLow) * (newHigh - newLow) / (currentHigh - currentLow);
     }
-    
+
     private static float getRed(int hex) {
         return ((hex >> 16) & 0xFF) / 255f;
     }
-    
+
     private static float getGreen(int hex) {
         return ((hex >> 8) & 0xFF) / 255f;
     }
-    
+
     private static float getBlue(int hex) {
         return ((hex) & 0xFF) / 255f;
     }
-    
+
     private static float getAlpha(int hex) {
         return ((hex >> 24) & 0xff) / 255f;
     }
-    
+
     private static float[] getARGB(int hex) {
-        return new float[] {getAlpha(hex), getRed(hex), getGreen(hex), getBlue(hex)};
+        return new float[]{getAlpha(hex), getRed(hex), getGreen(hex), getBlue(hex)};
     }
-    
+
     private static int toInt(float[] argb) {
         int r = (int) Math.floor(argb[1] * 255) & 0xFF;
         int g = (int) Math.floor(argb[2] * 255) & 0xFF;
@@ -84,25 +84,25 @@ public class AmbientEnvironment {
         int a = (int) Math.floor(argb[0] * 255) & 0xFF;
         return (a << 24) + (r << 16) + (g << 8) + (b);
     }
-    
+
     public static double clamp(double value, double min, double max) {
         return Math.max(min, Math.min(value, max));
     }
-    
+
     public static double curve(double start, double end, double amount, double waves) {
         amount = clamp(amount, 0, 1);
         amount = clamp((amount - start) / (end - start), 0, 1);
         return clamp(0.5 + 0.5 * Math.sin(Math.cos(Math.PI * Math.tan(90 * amount))) * Math.cos(Math.sin(Math.tan(amount))), 0, 1);
     }
-    
+
     public static int blend(int color1, int color2, float ratio) {
         float ir = 1.0f - ratio;
-        
+
         float[] rgb1 = getARGB(color2);
         float[] rgb2 = getARGB(color1);
-        
-        return toInt(new float[] {rgb1[0] * ratio + rgb2[0] * ir, rgb1[1] * ratio + rgb2[1] * ir, rgb1[2] * ratio + rgb2[2] * ir, rgb1[3] * ratio + rgb2[3] * ir});
-        
+
+        return toInt(new float[]{rgb1[0] * ratio + rgb2[0] * ir, rgb1[1] * ratio + rgb2[1] * ir, rgb1[2] * ratio + rgb2[2] * ir, rgb1[3] * ratio + rgb2[3] * ir});
+
     }
-    
+
 }
