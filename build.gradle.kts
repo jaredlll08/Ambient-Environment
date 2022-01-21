863debf75434de279ba7aa485868395c17277c20
Jared <jaredlll08@gmail.com>
2022-01-21 22:08:31 +0200

    pin the plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
 plugins {
     `java-library`
     idea
-    id("com.blamejared.modtemplate") version ("3.0.0.+")
+    id("com.blamejared.modtemplate") version ("3.0.0.37")
 }
 
 val minecraftVersion: String by project
