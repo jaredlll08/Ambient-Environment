7f7ee10cfadefd7af5b079c71b2cfe7d9734f0c2
Jared <jaredlll08@gmail.com>
2023-06-20 10:42:16 +0200

    port to 1.20.1

--- buildSrc/src/main/kotlin/com/blamejared/ambientenvironment/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/ambientenvironment/gradle/Versions.kt
@@ -1,9 +1,9 @@
 package com.blamejared.ambientenvironment.gradle
 
 object Versions {
-    const val MOD = "10.0.0"
+    const val MOD = "11.0.0"
     const val JAVA = "17"
-    const val MINECRAFT = "1.20"
-    const val FORGE = "46.0.14"
+    const val MINECRAFT = "1.20.1"
+    const val FORGE = "47.0.4"
     const val FABRIC_LOADER = "0.14.21"
 }
