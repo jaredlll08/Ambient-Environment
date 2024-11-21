e2781d6138003ae6c49df54e0927fbc0c916e8bf
Jared <jaredlll08@gmail.com>
2024-11-21 15:27:04 -0700

    Port to 1.21.2

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -1,13 +1,13 @@
 package com.blamejared
 
 object Versions {
-    const val MOD = "18.0.0"
+    const val MOD = "19.0.0"
     const val JAVA = "21"
-    const val MINECRAFT = "1.21.1"
-    const val FABRIC_LOADER = "0.15.11"
-    const val FABRIC = "0.102.1+1.21.1"
+    const val MINECRAFT = "1.21.2"
+    const val FABRIC_LOADER = "0.16.9"
+    const val FABRIC = "0.106.1+1.21.2"
     const val FORGE = "49.0.19" // 1.20.4
     const val FORGE_LOADER = "[49,)" // 1.20.4
-    const val NEO_FORGE = "21.1.8"
+    const val NEO_FORGE = "21.2.1-beta"
     const val NEO_FORGE_LOADER= "[4,)"
 }
