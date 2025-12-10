443152cef3a3293b43dc50a747f6fa4221f25be5
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-10 10:19:37 -0500

    update neoforge

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -6,6 +6,6 @@ object Versions {
     const val MINECRAFT = "1.21.11"
     const val FABRIC_LOADER = "0.18.1"
     const val FABRIC = "0.139.4+1.21.11"
-    const val NEO_FORGE = "21.10.1-beta"
+    const val NEO_FORGE = "21.11.0-beta"
     const val NEO_FORGE_LOADER= "[4,)"
 }
