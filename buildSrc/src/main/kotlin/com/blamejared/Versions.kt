23768a85fd2cbf2a0c7c122c141829585953f9fd
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-10 10:18:32 -0500

    update version to 1.21.11

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -3,7 +3,7 @@ package com.blamejared
 object Versions {
     const val MOD = "27.0.0"
     const val JAVA = "21"
-    const val MINECRAFT = "1.21.10"
+    const val MINECRAFT = "1.21.11"
     const val FABRIC_LOADER = "0.17.2"
     const val FABRIC = "0.134.1+1.21.10"
     const val NEO_FORGE = "21.10.1-beta"
