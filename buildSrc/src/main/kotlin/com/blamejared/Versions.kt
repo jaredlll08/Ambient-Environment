76acd27c2a792d12e57891d1fc8c4619bdd22474
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-10 10:19:10 -0500

    update fabric loader and fabric-api

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -4,8 +4,8 @@ object Versions {
     const val MOD = "27.0.0"
     const val JAVA = "21"
     const val MINECRAFT = "1.21.11"
-    const val FABRIC_LOADER = "0.17.2"
-    const val FABRIC = "0.134.1+1.21.10"
+    const val FABRIC_LOADER = "0.18.1"
+    const val FABRIC = "0.139.4+1.21.11"
     const val NEO_FORGE = "21.10.1-beta"
     const val NEO_FORGE_LOADER= "[4,)"
 }
